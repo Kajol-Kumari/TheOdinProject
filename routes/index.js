07dcf5c447c6d839ad62fd7598f7cd955179d97b
kajol <kajolkumarisingh222@gmail.com>
2020-01-17 02:30:04 +0530

    make it better

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
 
+const messages =[{text: "Hello there!", user:"Kajol", added: new Date()},
+{text:"Hello Node Developers!", user:"Kaju", added: new Date()}];
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Mini Messageboard',  messages: messages });
 });
 
+router.get('/new', function(req, res, next) {
+  res.render('form');
+})
+
+router.post('/new', function(req, res, next){
+  let newtext = req.body.msg;
+  let newuser= req.body.author;
+  messages.push({
+    text: newtext,
+    user: newuser,
+    added: new Date().toLocaleString()
+  });
+  res.redirect('/');
+})
 module.exports = router;
